b3fdf32a92a97584ed300df0cfd6a6f9f7b96058
Paul O'Leary <poleary@polygon.technology>
2023-06-10 18:10:19 -0700

    dotenv nonsense. still doesnt work :/

--- frontend/src/libs/magic.ts
+++ frontend/src/libs/magic.ts
@@ -3,7 +3,8 @@ import { Magic } from "magic-sdk"
 // Initialize the Magic instance
 export const magic = new Magic("pk_live_F4A72E6ABE77B965", {
   network: {
-    rpcUrl: "https://eth-sepolia.g.alchemy.com/v2/demo",
-    chainId: 11155111,
+    rpcUrl: process.env.NETWORK_RPC_URL || "https://rpc-mumbai.matic.today", // TODO: .env config not working!?!?
+    chainId: 80001, // Mumbai
   },
 })
+magic.network = 'matic';
