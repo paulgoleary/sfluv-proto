f399696739ea5903f4785c0abcf12ff35cf31d11
pjol <pj@pjs-macbook-pro.gateway.sonic.net>
2023-10-15 21:19:51 -0700

    Styled Frontend - web3auth popups currently blocked in build mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Web3  from 'web3';
 import auth from './web3auth';
 import { UserOperationBuilder } from 'userop';
 import links from './links';
+import logo from './imgs/SFLUV.svg'
 
 import './App.css';
 
@@ -13,24 +14,29 @@ function App() {
   const entryPoint = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
 
 
-  const [connected, setConnected] = useState(false);
-  const [account, setAccount] = useState('');
 
-  const [signatureMessage, setSignatureMessage] = useState('');
-  const [recoverMessage, setRecoverMessage] = useState('');
-  const [recoverSigned, setRecoverSigned] = useState('');
+  const [account, setAccount] = useState('');
+  var awaiting = false;
+
+  // const [signatureMessage, setSignatureMessage] = useState('');
+  // const [recoverMessage, setRecoverMessage] = useState('');
+  // const [recoverSigned, setRecoverSigned] = useState('');
+  const [unwrapAddress, setUnwrapAddress] = useState('');
+  const [unwrapAmount, setUnwrapAmount] = useState('');
+  const [advancedUnwrap, setAdvancedUnwrap] = useState(false);
 
 
 
 
   const connect = async () => {
-    await web3auth.connect();
-    setConnected(web3auth.connected);
+    awaiting = true;
+    if(!web3auth.connected) {
+      await web3auth.connect();
+    };
+    awaiting = false;
     if (web3auth.provider) {
       const w = new Web3(web3auth.provider)
       setWeb3(w);
-
-      console.log(w);
     }
   }
 
@@ -39,14 +45,14 @@ function App() {
   const disconnect = async () => {
     await web3auth.logout();
     setWeb3(undefined);
-    setConnected(await web3auth.connected);
   }
 
 
   const web3authSetup = async () => {
-    await web3auth.initModal();
-    setConnected(web3auth.connected);
-    if (!connected) {
+    if(!web3auth.connected) {
+      await web3auth.initModal();
+    };
+    if (!web3auth.connected) {
       connect();
     };
   }
@@ -78,8 +84,11 @@ function App() {
 
 
   useEffect(() => {
-    if (!connected) {
+    if (!web3auth.connected && !awaiting) {
       web3authSetup();
+    }
+    return () => {
+      web3auth.loginModal.closeModal();
     }
   }, []);
 
@@ -89,17 +98,15 @@ function App() {
     if (web3) {
       const privateKey = await getPrivateKey();
       const signedMessage = web3.eth.accounts.sign(message, '0x' + privateKey);
-      // console.log(signedMessage);
       return(signedMessage);
     }
   }
 
-  const recoverAddress = (message: string, signedMessage: string) => {
-    if (web3) {
-      const recoveredAddress = web3.eth.accounts.recover(message, signedMessage);
-      console.log(recoveredAddress);
-    }
-  }
+  // const recoverAddress = (message: string, signedMessage: string) => {
+  //   if (web3) {
+  //     const recoveredAddress = web3.eth.accounts.recover(message, signedMessage);
+  //   }
+  // }
 
   const fetchData = async (url, postData) => {
     var returnData = undefined;
@@ -109,8 +116,6 @@ function App() {
         throw new Error('Failed Fetch');
       }
       const response = res.json();
-      // console.log(response);
-      // console.log(res);
       if(response){
         return(response);
       } else {
@@ -118,7 +123,6 @@ function App() {
       }
     })
     .then(data => {
-      console.log(data);
       returnData = data;
     })
     return(returnData);
@@ -126,22 +130,20 @@ function App() {
 
   const signUserOperation = async (ctx) => {
     const hash = ctx.getUserOpHash();
-    // console.log(ctx);
-    console.log('Hash:' + hash);
     const signedMessage = await signMessage(hash);
-    // console.log(signedMessage);
     ctx.op.signature = signedMessage.signature;
   }
 
-  const makeUserOp = async () => {
-    const firstFetchData = await fetchData(l.initial + 'owner=' + account);
-    const userOpData = await fetchData(
-      l.unwrap +
-      'target=58a2993A618Afee681DE23dECBCF535A58A080BA&to=' +
-      account +
-      '&amount=1000000&owner=' +
-      account
-    );
+  const makeUserOp = async (amount, address) => {
+    // const firstFetchData = await fetchData(l.initial + 'owner=' + account);
+    const link = l.unwrap +
+    'target=58a2993A618Afee681DE23dECBCF535A58A080BA&to=' +
+    (address ? address : account) +
+    '&amount=' +
+    amount +
+    '&owner=' +
+    account;
+    const userOpData = await fetchData(link);
     const builder = new UserOperationBuilder()
       .setNonce(userOpData.nonce)
       .setSender(userOpData.sender)
@@ -156,14 +158,13 @@ function App() {
       .useMiddleware(signUserOperation)
     ;
     let op = await builder.buildOp(entryPoint , 137);
-    console.log(op);
     return(op);
 
   }
 
-  const unwrap = async () => {
-    const op = await makeUserOp();
-    console.log(op);
+  const unwrap = async (amount, address) => {
+    amount = amount * 1000000;
+    const op = await makeUserOp(amount, address);
     const postData = { entryPoint, op };
     console.log(postData);
     const post = await fetchData(l.post, {
@@ -175,12 +176,65 @@ function App() {
 
   return (
     <div className="App">
-      <p>Hi</p>
-      {connected && <div>
-
+      {!web3auth.connected && <img src={logo} alt='SFLUV' className='logo' id='logged-out-logo'></img>}
+      {web3auth.connected && <div id='unwrap-tool-title'>
+        <img src={logo} alt='SFLUV' className='logo' id='logged-in-logo'></img>
+        <div id='unwrap-tool-title-text'><h1 className='title'>Unwrap Tool</h1></div>
+      </div>}
+      {/* <p id='address'>{account}</p> */}
+      {web3auth.connected && <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            unwrap(unwrapAmount, unwrapAddress);
+          }}
+          id='unwrap-form'>
+          <input
+            type='number'
+            step='.000001'
+            onWheel={e => {
+              e.currentTarget.blur();
+            }}
+            id='unwrap-amount-input'
+            className='single-input'
+            onChange={(e) => {
+              setUnwrapAmount(e.target.value);
+            }}
+            placeholder='AMOUNT'
+          ></input>
+          <br/>
+          <button id='unwrap-button' className='submit-button'>Unwrap</button>
+          <br/>
+          <button
+            className='dropdown-button'
+            id='advanced-unwrap-dropdown'
+            onClick={(e) => {
+              e.preventDefault();
+              if (advancedUnwrap) {
+                setAdvancedUnwrap(false);
+              } else {
+                setAdvancedUnwrap(true);
+              }
+            }}
+          >
+            {advancedUnwrap ? 'v Advanced' : '> Advanced'}
+          </button>
+          <br/>
+          {advancedUnwrap && <div>
+            <input
+            type='text'
+            id='unwrap-address-input'
+            className='single-input'
+            onChange={(e) => {
+              setUnwrapAddress(e.target.value);
+            }}
+            placeholder='ADDRESS'
+            ></input>
+            <br/>
+          </div>}
+        </form>
         <button onClick={disconnect}>Disconnect</button>
-        <br/>
-        <button onClick={getAccounts}>Get Accounts</button>
+        {/* <button onClick={getAccounts}>Get Accounts</button>
         <form onSubmit={(e) => {
           e.preventDefault();
           signMessage(signatureMessage);
@@ -191,16 +245,12 @@ function App() {
             onChange={(e) => setSignatureMessage(e.target.value)}>
           </input>
           <button>Sign Message</button>
-        </form>
-        <button onClick={unwrap}>Unwrap</button>
-
+        </form> */}
       </div>}
-      {!connected && <div>
-
+      {!web3auth.connected && <div>
         <button onClick={connect}>Connect</button>
-
       </div>}
-      <form onSubmit={(e) => {
+      {/* <form onSubmit={(e) => {
           e.preventDefault();
           recoverAddress(recoverMessage, recoverSigned);
         }}>
@@ -218,8 +268,7 @@ function App() {
           </input>
           <br></br>
           <button>Recover Address</button>
-        </form>
-        <p>Wallet account: {account}</p>
+        </form> */}
     </div>
   );
 }
