b86684574a3ffd72ddb06864840a38eebcd0b058
Paul O'Leary <poleary@polygon.technology>
2024-01-20 10:45:05 -0800

    WIP - research account delegates.

---
+++ contracts/src/SFLUVAccount.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Account} from "../lib/smartcontracts/contracts/accounts/Account.sol";
+import {ITokenEntryPoint} from "../lib/smartcontracts/contracts/accounts/interfaces/ITokenEntryPoint.sol";
+import {TokenEntryPoint} from "../lib/smartcontracts/contracts/accounts/TokenEntryPoint.sol";
+
+import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+contract SFLUVAccount is Account {
+    constructor(IEntryPoint anEntryPoint, ITokenEntryPoint aTokenEntryPoint)
+    Account(anEntryPoint, aTokenEntryPoint) {}
+}
