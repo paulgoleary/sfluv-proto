78135dc06215077ba6bb55796098b070f310b125
PJ O'Leary <peej@oleary.com>
2024-01-24 18:15:49 -0800

    W3A redirect & styling

--- frontend/src/web3Auth.js
+++ frontend/src/web3Auth.js
@@ -2,6 +2,7 @@ import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { CHAIN_NAMESPACES } from "@web3auth/base";
 import { Web3AuthNoModal } from "@web3auth/no-modal";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import Logo from '../assets/SFLUV LOGO-1.svg';
 import 'dotenv';
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
@@ -22,6 +23,21 @@ const privateKeyProvider = new EthereumPrivateKeyProvider({
 
 const openloginAdapter = new OpenloginAdapter({
   privateKeyProvider: privateKeyProvider,
+  adapterSettings: {
+    uxMode: "redirect",
+    whiteLabel: {
+      appName: "SFLUV",
+      appUrl: "https://web3auth.io",
+      logoLight: Logo,
+      logoDark: Logo,
+      defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+      mode: "light", // whether to enable dark mode. defaultValue: auto
+      theme: {
+        primary: "#whitesmoke",
+      },
+      useLogoLoader: true,
+    },
+  }
 });
 
 const web3auth = new Web3AuthNoModal({
